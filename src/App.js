4be1b34ad047f953bbcf2be45a87dff2403930b8
SkandarEverest <skandar.everest@gmail.com>
2022-07-06 11:41:42 +0700

    fix merge conflicts

--- src/App.js
+++ src/App.js
@@ -2,10 +2,7 @@ import NavigationBar from "./components/NavigationBar";
 import Artikel from "./components/Artikel";
 import Informasi from "./components/Informasi";
 import Footer from "./components/Footer";
-<<<<<<< HEAD
-=======
 import News from "./components/Article";
->>>>>>> 17b8f96cf0cd1e1cb0381b5be44ccc41844d5e08
 
 function App() {
   return (
